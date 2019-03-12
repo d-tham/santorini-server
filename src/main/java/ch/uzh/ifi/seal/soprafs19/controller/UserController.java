38d530bbf3b96bacc26de252665fda6b53727971
David Tham <tham.david@gmx.ch>
2019-03-12 23:58:50 +0100

    Fixed the REST API specification. PUT users/{userId} now returns nothing.

--- src/main/java/ch/uzh/ifi/seal/soprafs19/controller/UserController.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/controller/UserController.java
@@ -3,10 +3,9 @@ package ch.uzh.ifi.seal.soprafs19.controller;
 import ch.uzh.ifi.seal.soprafs19.entity.User;
 import ch.uzh.ifi.seal.soprafs19.repository.UserRepository;
 import ch.uzh.ifi.seal.soprafs19.service.UserService;
-import ch.uzh.ifi.seal.soprafs19.exceptions.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class UserController {
@@ -23,38 +22,44 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    @ExceptionHandler({UserAlreadyExistsException.class})
+    @ResponseStatus(HttpStatus.CREATED)
     User createUser(@RequestBody User newUser) {
         if (this.service.getUserByUsername(newUser.getUsername()) != null) {
-            throw new UserAlreadyExistsException("User already exists!"); }
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "User already exists!"); }
 
         return this.service.createUser(newUser);
     }
 
     @GetMapping("/users/{userId}")
-    @ExceptionHandler({UserAlreadyExistsException.class})
+    @ResponseStatus(HttpStatus.OK)
     User single(@PathVariable("userId") long userId) {
         if (this.service.getUserByUserId(userId) == null) {
-            throw new UserNotFoundException("User was not found!"); }
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User was not found!"); }
 
-        return service.getUserByUserId(userId);}
+        return this.service.getUserByUserId(userId);}
 
     @PostMapping("/login")
-    @ExceptionHandler({UserNotFoundException.class})
+    @ResponseStatus(HttpStatus.OK)
     User loginUser(@RequestBody User tempUser) {
         if (this.service.getUserByUsername(tempUser.getUsername()) == null) {
-            throw new UserNotFoundException("User was not found!"); }
-
-        return service.loginUser(tempUser.getUsername(), tempUser.getPassword());
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User was not found!"); }
+        else if (this.service.loginUser(tempUser.getUsername(), tempUser.getPassword()) == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Wrong login credentials!");
+        }
+        return this.service.loginUser(tempUser.getUsername(), tempUser.getPassword());
     }
 
     @CrossOrigin
     @PutMapping("/users/{userId}")
-    User updateUser(@PathVariable("userId") long userId, @RequestBody User tempUser) {
-        if (!this.service.getUserByUserId(userId).getToken().equals(tempUser.getToken())) {
-            throw new RuntimeException("NOT AUTHORIZED");
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void updateUser(@PathVariable("userId") long userId, @RequestBody User tempUser) {
+        if (this.service.getUserByUserId(userId) == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User was not found!");
+        } else if (!this.service.getUserByUserId(userId).getToken().equals(tempUser.getToken())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Not authorized!");
+        } else {
+            this.service.updateUser(userId, tempUser.getUsername(), tempUser.getBirthDate());
         }
-        return this.service.updateUser(userId, tempUser.getUsername(), tempUser.getBirthDate());
     }
 
 }
